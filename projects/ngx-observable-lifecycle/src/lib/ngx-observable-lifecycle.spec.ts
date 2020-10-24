f0dd22072ec03a6a5632d924a5e14148fbcf17d1
Zak Henry <zak@cloudnc.com>
2020-10-24 15:59:07 +0100

    fix(Types): Restructure types for correctness, increase code coverage to 100%

--- projects/ngx-observable-lifecycle/src/lib/ngx-observable-lifecycle.spec.ts
+++ projects/ngx-observable-lifecycle/src/lib/ngx-observable-lifecycle.spec.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { isObservable } from 'rxjs';
 import { getObservableLifecycle } from './ngx-observable-lifecycle';
 
@@ -36,6 +36,53 @@ describe('ngx-observable-lifecycle', () => {
       const unobservedInstance = new TestClass();
 
       expect(() => unobservedInstance.ngOnDestroy()).not.toThrowError();
+    });
+
+    it('should re-use observer subjects when a hook is observed multiple times', () => {
+      const { ngOnInit: firstObservable } = getObservableLifecycle(componentInstance);
+      const { ngOnInit: secondObservable } = getObservableLifecycle(componentInstance);
+
+      expect(firstObservable.source).toBe(secondObservable.source);
+    });
+
+    it('invokes the original method if present', () => {
+      const originalOnDestroySpy = jasmine.createSpy();
+
+      class TestClass implements OnDestroy {
+        public ngOnDestroy(): void {
+          originalOnDestroySpy();
+        }
+      }
+
+      const instance = new TestClass();
+
+      const { ngOnDestroy } = getObservableLifecycle(instance);
+
+      expect(originalOnDestroySpy).not.toHaveBeenCalled();
+
+      instance.ngOnDestroy();
+
+      expect(originalOnDestroySpy).toHaveBeenCalled();
+    });
+
+    it('does not need the original hook(s) to be defined', () => {
+      const onInitSpy = jasmine.createSpy();
+
+      class TestClass {}
+
+      const instance = new TestClass();
+
+      const { ngOnInit } = getObservableLifecycle(instance);
+
+      ngOnInit.subscribe(onInitSpy);
+
+      expect(onInitSpy).not.toHaveBeenCalled();
+
+      (instance as OnInit).ngOnInit();
+
+      expect(onInitSpy).toHaveBeenCalled();
+
+      (instance as OnDestroy).ngOnDestroy();
     });
   });
 });
